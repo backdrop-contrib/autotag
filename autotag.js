176532288a3175a9b0be2f75e688332b495b98ba
Simon Rycroft <drupal.org@simonrycroft.com>
2008-06-05 11:25:54 +0000

    First commit of autotag module

---
+++ autotag.js
@@ -0,0 +1,50 @@
+// On initial load, add dragable to all li dragable items
+$(document).ready(function () {
+  // Prevent duplicate attachment of the collapsible behavior.
+  $ui('.autotag-dragable').draggable({helper: 'clone'});
+  $ui('#autotag-dnd-right').droppable({
+  	accept: ".autotag-dragable",
+  	activeClass: 'autotag-dnd-drop-active',
+  	hoverClass: 'autotag-dnd-drop-hover',
+  	drop: function(ev, ui) {
+  		ui.draggable.appendTo("#autotag-dnd-right>ul");
+  	}
+  });
+  $ui('#autotag-dnd-left').droppable({
+  	accept: ".autotag-dragable",
+  	activeClass: 'autotag-dnd-drop-active',
+  	hoverClass: 'autotag-dnd-drop-hover',
+  	drop: function(ev, ui) {
+  		ui.draggable.appendTo("#autotag-dnd-left>ul");
+  	}
+  });
+  $ui('#autotag-dnd-right>*').droppable({
+  	accept: ".autotag-dragable",
+  	activeClass: 'autotag-dnd-drop-active',
+  	hoverClass: 'autotag-dnd-drop-hover',
+  	drop: function(ev, ui) {
+  		ui.draggable.appendTo("#autotag-dnd-right>ul");
+  	}
+  });
+  $ui('#autotag-dnd-left>*').droppable({
+  	accept: ".autotag-dragable",
+  	activeClass: 'autotag-dnd-drop-active',
+  	hoverClass: 'autotag-dnd-drop-hover',
+  	drop: function(ev, ui) {
+  		ui.draggable.appendTo("#autotag-dnd-left>ul");
+  	}
+  });
+  $ui('.autotag-dragable').droppable({
+  	accept: ".autotag-dragable",
+  	drop: function(ev, ui) {
+  		ui.draggable.appendTo($(this).parent());
+  	}
+  });
+});
+function autotag_update_form(){
+  var hidden_tids = '';
+  $.each($ui('#autotag-dnd-right>ul>*'), function(){
+    hidden_tids += '|'+$ui(this).attr('tid');
+  });
+  $('#edit-hideterms').val(hidden_tids);
+}
