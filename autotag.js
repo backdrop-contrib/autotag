09e58fa100d660470d62ad2d7b2dad2865f28fce
Simon Rycroft <drupal.org@simonrycroft.com>
2010-09-23 09:33:52 +0000

    Updated to V2

--- autotag.js
+++ autotag.js
@@ -1,57 +1,26 @@
-// On initial load, add dragable to all li dragable items
-$(document).ready(function () {
-  // Prevent duplicate attachment of the collapsible behavior.
-  $('.autotag-dragable').draggable({
-    helper: 'clone', 
-    zIndex: 150,
-    appendTo: '#autotag-container',
-  });
-  $('#autotag-dnd-right').droppable({
-    accept: ".autotag-dragable",
-    activeClass: 'autotag-dnd-drop-active',
-    hoverClass: 'autotag-dnd-drop-hover',
-    drop: function(ev, ui) {
-      ui.draggable.appendTo("#autotag-dnd-right>ul");
+/*
+ * Define a function that is called when the ahah action is called 
+ * (beforeSubmit).  This funciton will update the values for the WYSIWYG form
+ * fields, which is done on a normal submit.
+ */
+function autotag_submitextra(form_values, element, options){
+  $.each(Drupal.wysiwyg.instances, function(index, value){
+    if(value.status != undefined && value.status == 1){
+      var editor = tinyMCE.get(value.field);
+      editor.save();
+      $.each(form_values, function(i, m){
+        if(m.name == $('#'+value.field).attr('name')){
+          form_values[i]['value'] = $('#'+value.field).val();
+        }
+      });
     }
   });
-  $('#autotag-dnd-left').droppable({
-    accept: ".autotag-dragable",
-    activeClass: 'autotag-dnd-drop-active',
-    hoverClass: 'autotag-dnd-drop-hover',
-    drop: function(ev, ui) {
-      ui.draggable.appendTo("#autotag-dnd-left>ul");
-    }
-  });
-  $('#autotag-dnd-right>*').droppable({
-    accept: ".autotag-dragable",
-    activeClass: 'autotag-dnd-drop-active',
-    hoverClass: 'autotag-dnd-drop-hover',
-    drop: function(ev, ui) {
-      ui.draggable.appendTo("#autotag-dnd-right>ul");
-    }
-  });
-  $('#autotag-dnd-left>*').droppable({
-    accept: ".autotag-dragable",
-    activeClass: 'autotag-dnd-drop-active',
-    hoverClass: 'autotag-dnd-drop-hover',
-    drop: function(ev, ui) {
-      ui.draggable.appendTo("#autotag-dnd-left>ul");
-    }
-  });
-  $('.autotag-dragable').droppable({
-    accept: ".autotag-dragable",
-    drop: function(ev, ui) {
-      ui.draggable.appendTo($(this).parent());
-    }
-  });
-  $('#autotag_remove_all').bind('click', function (){
-    $('#autotag-dnd-left>ul>li').appendTo("#autotag-dnd-right>ul")
-  });
-});
-function autotag_update_form(){
-  var hidden_tids = '';
-  $.each($('#autotag-dnd-right>ul>*'), function(){
-    hidden_tids += '|'+$(this).attr('tid');
-  });
-  $('#edit-hideterms').val(hidden_tids);
+}
+/*
+ * Uncheck the autotag check box
+ */
+function autotag_uncheckbox(message){
+  $('#edit-taxonomyautotagcheckbox').attr('checked', '');
+  $('#autotag-message').html(message);
+  $('#autotag-message').slideDown();
 }
